b67f8c647e93265af365d8e44174ed85724efabd
Fernão Guerra <fernao@fernao-macbookpro.roam.corp.google.com>
2020-05-22 01:32:29 -0300

    first commit

---
+++ twitterbot.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+
+while 1:
+    page = requests.get("https://www.worldometers.info/coronavirus/country/brazil/")
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    container = soup.findAll("div", {"class": "maincounter-number"})
+    cases = container[0].text.strip()
+    deaths = container[1].text.strip()
+    recovered = container[2].text.strip() 
+
+    brasil = ("🇧🇷 Total de casos no Brasil: \n"+ \
+            "Casos confirmados: " + cases + "\n" + \
+            "Óbitos: " + deaths+ "\n" + \
+            "Recuperações: " + recovered + "\n\n")
+
+    page = requests.get("https://www.worldometers.info/coronavirus/")
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    container = soup.findAll("div", {"class": "maincounter-number"})
+    cases = container[0].text.strip()
+    deaths = container[1].text.strip()
+    recovered = container[2].text.strip() 
+
+    mundo = ("🌍 Total de casos no Mundo: \n"+ \
+            "Casos confirmados: " + cases + "\n" + \
+            "Óbitos: " + deaths+ "\n" + \
+            "Recuperações: " + recovered + "\n\n")
+
+    print(brasil)
+    print(mundo)
+
+    url = "https://hooks.zapier.com/hooks/catch/7023378/o1p2v85/"
+    data = {'value1': brasil , 'value2': mundo}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    print(r.text)
+    print("dormindo por 60 minutos")
+    time.sleep(3600)
